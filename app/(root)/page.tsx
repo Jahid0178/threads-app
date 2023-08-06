b8ff6ebef2625ca66c738a6895474a2a2cf2df51
Jahid0178 <zahidul.haque767@gmail.com>
2023-08-06 17:13:14 +0600

    Added create users using clerk authentication
    - Modify root and auth layout added clerk provider for authentication
    
    - Created sign in and sign up page with clerk
    
    - Added clerk middleware
    
    - Added root file .env.local

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello Next.js</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
