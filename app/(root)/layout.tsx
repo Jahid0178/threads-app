cbec309dc776e2d0fc80b2509e273a16cfef36d0
Jahid0178 <zahidul.haque767@gmail.com>
2023-08-06 17:25:48 +0600

    Added few components
    - Added right sidebar component
    
    - Added bottom bar component
    
    - Added Topbar component
    
    - Added Left side bar component
    
    - Added these component in root layout page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "../globals.css";
+
+import Topbar from "@/components/shared/Topbar/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +23,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Hello Next.js</h1>
-      <UserButton afterSignOutUrl="/" />
-    </main>
+    </>
   );
 }
