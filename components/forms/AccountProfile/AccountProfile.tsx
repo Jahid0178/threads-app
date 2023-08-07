5a9b8617705bdd3e81b3a8646ef32bcb36a66a12
Jahid0178 <zahidul.haque767@gmail.com>
2023-08-08 03:52:06 +0600

    Modify and update
    - Modify data transfer in db from account profile page
    
    - Modify user action files

--- components/forms/AccountProfile/AccountProfile.tsx
+++ components/forms/AccountProfile/AccountProfile.tsx
@@ -18,6 +18,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface AccountProfileProps {
   user: {
@@ -34,6 +36,8 @@ interface AccountProfileProps {
 const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -81,6 +85,20 @@ const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
     }
 
     // TODO: Update user profile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
   }
   return (
     <Form {...form}>
