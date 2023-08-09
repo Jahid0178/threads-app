e95c11a8fea527cf29caaeb8a43a394addf8eff9
Jahid0178 <zahidul.haque767@gmail.com>
2023-08-10 02:48:52 +0600

    ✨ Added search query functionality in search bar component

--- components/shared/Searchbar/Searchbar.tsx
+++ components/shared/Searchbar/Searchbar.tsx
@@ -1,13 +1,30 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
 
 interface SearchbarProps {
   routeType: string;
 }
 
 const Searchbar = ({ routeType }: SearchbarProps) => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        router.push(`/${routeType}?q=` + search);
+      } else {
+        router.push(`/${routeType}`);
+      }
+    });
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, routeType]);
+
   return (
     <div className="searchbar">
       <Image
@@ -22,6 +39,7 @@ const Searchbar = ({ routeType }: SearchbarProps) => {
         placeholder={`${
           routeType !== "search" ? "Search Communitites" : "Search Creators"
         }`}
+        onChange={(e) => setSearch(e.target.value.toLocaleLowerCase())}
       />
     </div>
   );
