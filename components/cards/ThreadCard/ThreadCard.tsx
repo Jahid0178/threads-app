f4f9258a88f385f90f922130636eed4bc4b1b960
Jahid0178 <zahidul.haque767@gmail.com>
2023-08-09 22:52:46 +0600

    ✨ Featured added and 💄 Modify UI
    💄 Modify Thread Card Component
    
    🙈 Added Clerk hook secret in .env.local file
    
    ✨ Community Post add featured added
    
    🎨 Updated user.action, thread.action and PostThread form
    
    ✅ Finally user can post in community or groups

--- components/cards/ThreadCard/ThreadCard.tsx
+++ components/cards/ThreadCard/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -110,7 +111,26 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+        {/* Delete Thread */}
+        {/* Show comment logos */}
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createAt)} - {community.name} Community{" "}
+          </p>
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={40}
+            height={40}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
